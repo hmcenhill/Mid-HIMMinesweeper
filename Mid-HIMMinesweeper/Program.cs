9c1db0c9fc63d4bf21d97032795dabe4f814ad7e
Hermann I McEnhill <hmcenhill@gmail.com>
2019-05-21 09:13:36 -0400

    VS files added

---
+++ Mid-HIMMinesweeper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mid_HIMMinesweeper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
